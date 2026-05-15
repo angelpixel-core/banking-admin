ab28ad954fd8886d6dded0d8c065891a05839c50
Angel Szymczak <contact@angelpixel.io>
2026-05-15 02:50:09 -0300

    test: add system smoke placeholder for CI

---
+++ spec/system/smoke_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "System smoke", type: :system do
+  it "runs system test suite scaffold" do
+    expect(true).to be(true)
+  end
+end
